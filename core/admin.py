a3cad985e39f5322ec6f6303e36e7d85f78a3f8a
marcelo.atsilva <marcelotoledo010@gmail.com>
2024-07-24 22:14:06 -0300

    24/07

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Products, Blog
+from .models import Products, Blog, Category
 
-admin.site.register(Products)
 
 
 #admin.site.register(Blog)
+@admin.register(Products)
+class Products(admin.ModelAdmin):
+    # form = BlogAdminForm # Estilização do Form Blog do Admin
+    list_display = ['name', 'mini_image']
+    search_fields = ['name']
+
 @admin.register(Blog)
 class Blog(admin.ModelAdmin):
     # form = BlogAdminForm # Estilização do Form Blog do Admin
     list_display = ['mini_image', 'blo_title']
     search_fields = ['blo_title']
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ['cat_name', 'slug']
+    search_fields = ['cat_name']
